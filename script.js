b535d9ed3df117be9e5f0cc5406d83b217055010
A Hassan <302795718@student.rocmondriaan.nl>
2021-03-24 22:21:53 +0100

    portfolio vernieuwd

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const elements=document.querySelectorAll('.text-center');
-const button=document.querySelector('#themeButton');
-if (button){
-button.addEventListener("click", function(){
-    for( let i=0 ; i<elements.length ; i++){
-        elements[i].classList.toggle("darkTheme")
+const button =document.querySelector('#modeSwitcher');
+const elements=document.querySelectorAll('.normalTheme ');
+button.addEventListener("click",function(){
+    for(let i=0 ; i<elements.length ;i++){
+        elements[i].classList.toggle("themedTexts");
     }
-    document.body.classList.toggle("darkBackground")
+    document.body.classList.toggle("bodyBackgroundColor");
 })
-}
