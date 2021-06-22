3cf25e2ed9e571ddde59d7e1b4fce9c47f94c1ce
Allan Hassan <302795718@student.rocmondriaan.nl>
2021-06-22 13:04:39 +0200

    portfolio omgezet naar git

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const button =document.querySelector('#modeSwitcher');
-const elements=document.querySelectorAll('.normalTheme ');
-button.addEventListener("click",function(){
-    for(let i=0 ; i<elements.length ;i++){
-        elements[i].classList.toggle("themedTexts");
+//collapsable navbar
+const navbarToggler=document.querySelector(".navbar-toggler");
+const navbar=document.querySelector(".navbar");
+const ul=document.querySelector("ul");
+navbarToggler.addEventListener("click",()=>{
+    navbar.classList.toggle("collapsed-navbar");
+    ul.classList.toggle("collapsed-ul");
+})
+//sticky navbar
+const sticky=navbar.offsetTop;
+window.onscroll = ()=>{
+    if(window.pageYOffset>=sticky){
+        navbar.classList.add("sticky")
     }
-    document.body.classList.toggle("bodyBackgroundColor");
+    else{
+        navbar.classList.remove("sticky")
+    }
+};
+//theme switcher
+const texts=document.querySelectorAll("h2, h3, h4, h5, h6, p, label, input, .fa, .contact-links, .gmail-link, .facebook-link, .phone-num");
+const themeToggler=document.querySelector(".theme-toggler");
+themeToggler.addEventListener("click",()=>{
+    for(let i=0 ;i<texts.length;i++){
+        texts[i].classList.toggle("themed-texts");
+    }
+    document.body.classList.toggle("themed-body");
 })
